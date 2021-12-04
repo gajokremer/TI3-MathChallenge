7ee8a75dd15416ed1df21b7e87d2207b9a100a89
Gabriel Kremer <gajokremer@gmail.com>
2021-12-03 21:24:35 -0500

    All operations can now be generated

--- src/model/GameManager.java
+++ src/model/GameManager.java
@@ -3,6 +3,7 @@ package model;
 public class GameManager {
 	
 	private Player root;
+	private String currentQuestion;
 	
 	public GameManager() {
 		
@@ -14,5 +15,205 @@ public class GameManager {
 
 	public void setRoot(Player root) {
 		this.root = root;
+	}
+	
+	public String getCurrentQuestion() {
+		return currentQuestion;
+	}
+
+	public void setCurrentQuestion(String currentQuestion) {
+		this.currentQuestion = currentQuestion;
+	}
+
+	public String[] newProblem() {
+		
+		int type = generateType();
+		
+		generateProblem(type, true);
+
+//		int[] values = generateValues();
+//		String equation = generateQuestion(values[0], values[1]);
+//		System.out.println("Equation: " + equation);
+//		return generateAnswer(equation);
+		
+		return null;
+	}
+	
+	private int generateType() {
+		
+//		int type = (int) (Math.random() * (4 - 1) + 1);
+		int type = 4;
+		
+		return type;
+	}
+
+	
+	private void generateProblem(int type, boolean valid) {
+
+		valid = true;
+		
+		if(valid) {
+			
+			int[] values = generateValues();
+			int a = values[0];
+			int b = values[1];
+			
+			if(type != 4) {
+				
+				String equation = generateQuestion(a, b, type);
+				generateAnswer(equation);
+				
+			} else if(type == 4) {
+				
+				valid = isValid(a, b);
+				
+				if(valid) {
+					
+					String equation = generateQuestion(a, b, type);
+					generateAnswer(equation);
+					
+				} else {
+					
+					generateProblem(type, valid);
+				}
+			}
+			
+		}
+	}
+
+	private int[] generateValues() {
+
+		int[] values = new int[2];
+
+		int a = (int) (Math.random() * 99);
+		int b = (int) (Math.random() * 99);
+		
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+
+		values[0] = a;
+		values[1] = b;
+		
+		return values;
+	}
+	
+	private String generateQuestion(int a, int b, int type) {
+		
+		String result = "";
+//
+//		int a = (int) (Math.random() * 99);
+//		int b = (int) (Math.random() * 99);
+//		
+//		System.out.println("a: " + a);
+//		System.out.println("b: " + b);
+//
+//		int type = (int) (Math.random() * (4 - 1) + 1);
+//		int type = 4;
+//		System.out.println(type);
+		
+		switch(type) {
+		
+		case 1:
+//			result = a + " + " + b;
+			result = sum(a, b);
+			break;		
+			
+		case 2:
+//			result = a + " - " + b;
+			result = sub(a, b);
+			break;
+		
+		case 3:
+//			result = a + " * " + b;
+			result = mult(a, b);
+			break;
+
+		case 4:
+//			result = a + " / " + b;
+			result = div(a, b);
+			break;
+		}
+		
+		setCurrentQuestion(result);
+		
+		return result;
+	}
+	
+	private boolean isValid(int a, int b) {
+		
+		if(a != 0 && b != 0) {
+			
+			if(a > b || a == b) {
+				
+				if(a % b == 0) {
+					
+					return true;
+					
+				} else {
+					
+					return false;
+				}
+				
+			} else {
+				
+				if(b % a == 0) {
+					
+					return true;
+					
+				} else {
+					
+					return false;
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	private String sum(int a, int b) {
+
+		return a + " + " + b;
+	}
+	
+	private String sub(int a, int b) {
+
+		if(a > b || a == b) {
+			
+			return a + " - " + b;
+			
+		} else {
+			
+			return b + " - " + a;
+		}
+	}
+	
+	private String mult(int a, int b) {
+
+		return a + " * " + b;
+	}
+	
+	private String div(int a, int b) {
+		
+		if(a > b || a == b) {
+			
+			return a + "/" + b;
+		}
+		
+		else {
+			
+			return b + "/" + a;
+		}
+		
+		
+	}
+	
+	private String[] generateAnswer(String equation) {
+
+		return null;
+	}
+
+	public void addOrdered(String name) {
+		
+		
 	}
 }
--- src/ui/ControllerAdminGUI.java
+++ src/ui/ControllerAdminGUI.java
@@ -5,7 +5,12 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -31,6 +36,33 @@ public class ControllerAdminGUI {
 	
     @FXML
     private TextField tfNewPlayerName;
+    
+    @FXML
+    private Label lbTimer;
+    
+    @FXML
+    private Label lbPlayingNow;
+    
+    @FXML
+    private Label lbCurrentPoints;
+    
+    @FXML
+    private TextField tfProblem;
+    
+    @FXML
+    private ToggleGroup tgAnswers;
+    
+    @FXML
+    private RadioButton rbAnswer1;
+    
+    @FXML
+    private RadioButton rbAnswer2;
+    
+    @FXML
+    private RadioButton rbAnswer3;
+    
+    @FXML
+    private RadioButton rbAnswer4;
 
 	@FXML
 	void start() throws IOException {
@@ -46,9 +78,32 @@ public class ControllerAdminGUI {
 	}
 	
 	@FXML
-	void btnPlay() {
-
+	void btnPlay() throws IOException {
 		
+		if(!tfNewPlayerName.getText().trim().isEmpty()) {
+			
+			FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("Questions.fxml"));
+			fxmlloader.setController(this);
+			Parent menu = fxmlloader.load();
+			mainPane.getChildren().setAll(menu);
+			
+			Image logo = new Image("Math Challenge Logo.png");
+			ivMainMenuLogo.setImage(logo);
+			lbPlayingNow.setText("Player: " + tfNewPlayerName.getText());
+			lbCurrentPoints.setText("Points: " + 0);
+			
+			gm.newProblem();
+			String question = gm.getCurrentQuestion();
+			
+			tfProblem.setText(question);
+			
+		} else {
+			
+			String header = "Game error";
+			String message = "A name must be given to start the game";
+			
+			showWarningDialogue(header, message);
+		}
 	}
 	
 	@FXML
@@ -56,4 +111,34 @@ public class ControllerAdminGUI {
 
 		
 	}
+	
+	@FXML
+	void btnBack() throws IOException {
+		
+		FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
+		fxmlloader.setController(this);
+		Parent menu = fxmlloader.load();
+		mainPane.getChildren().setAll(menu);
+		
+		Image logo = new Image("Math Challenge Logo.png");
+		ivMainMenuLogo2.setImage(logo);
+	}
+	
+	public void showSuccessDialogue(String header, String message) {
+
+    	Alert alert = new Alert(AlertType.INFORMATION);
+    	alert.setTitle("Math Challenge");
+    	alert.setHeaderText(header);
+    	alert.setContentText(message);
+    	alert.showAndWait();
+    }
+    
+    public void showWarningDialogue(String header, String message) {
+    	
+    	Alert alert = new Alert(AlertType.WARNING);
+    	alert.setTitle("Math Challenge");
+    	alert.setHeaderText(header);
+    	alert.setContentText(message);
+    	alert.showAndWait();
+    }
 }
